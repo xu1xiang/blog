f184cff2905a419b17904408aab74e8015a847f8
volerx <volerx@users.noreply.github.com>
2021-08-20 08:40:07 +0000

    deploy: aef5c0d7b3aa4becefc075a0e9f7f023eb3e4d08

--- _next/static/Hd5jnoWIEiTVhIF-oVBEp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f3da4942e11b1c887833.css","static/chunks/pages/index-d3002838a726e0787ef8.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
