24332997f592aadda3aeab55a125c764d0669db4
Shaul Xu <302763630@qq.com>
2022-04-13 15:08:55 +0800

    config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,6 +6,7 @@ import { navbar, sidebar } from './configs'
 const isProd = process.env.NODE_ENV === 'production'
 
 export default defineUserConfig<GungnirThemeOptions>({
+  dest: 'build',
   base: '/',
   head: [
     [
@@ -26,16 +27,24 @@ export default defineUserConfig<GungnirThemeOptions>({
         href: `/img/logo/favicon-32x32.png`,
       },
     ],
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
-    ['meta', { name: 'application-name', content: 'Gungnir Theme' }],
-    ['meta', { name: 'apple-mobile-web-app-title', content: 'Gungnir Theme' }],
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content: "Shaul,Shaul's Blog,Shaul Blog",
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'baidu-site-verification',
+        content: 'code-ZU4gfKUMfB',
+      },
+    ],
+    ['meta', { name: 'apple-mobile-web-app-title', content: "Shaul's Blog" }],
     [
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
-    ],
-    [
-      'link',
-      { rel: 'apple-touch-icon', href: `/img/logo/apple-touch-icon.png` },
     ],
     ['meta', { name: 'theme-color', content: '#377bb5' }],
     ['meta', { name: 'msapplication-TileColor', content: '#377bb5' }],
@@ -45,13 +54,8 @@ export default defineUserConfig<GungnirThemeOptions>({
   locales: {
     '/': {
       lang: 'en-US',
-      title: 'VuePress Theme Gungnir',
-      description: 'A blog theme for VuePress',
-    },
-    '/zh/': {
-      lang: 'zh-CN',
-      title: 'VuePress Theme Gungnir',
-      description: 'VuePress 博客主题',
+      title: 'Shaul',
+      description: 'Shaul Blog',
     },
   },
 
@@ -64,29 +68,16 @@ export default defineUserConfig<GungnirThemeOptions>({
   theme: 'vuepress-theme-gungnir',
 
   themeConfig: {
-    repo: 'Renovamen/vuepress-theme-gungnir',
-    docsDir: 'docs',
-
     hitokoto: 'https://v1.hitokoto.cn?c=i', // enable hitokoto (一言) or not?
 
     // personal information
     personalInfo: {
-      name: 'Gungnir',
+      name: 'Shaul',
       avatar: '/img/avatar.jpeg',
-      description: 'A blog theme for VuePress',
+      description: "Shaul's Blog",
       sns: {
-        github: 'Renovamen',
-        linkedin: 'xiaohan-zou-55bba0160',
-        facebook: 'renovamen.zou',
-        twitter: 'renovamen_zxh',
-        zhihu: 'chao-neng-gui-su',
-        email: 'renovamenzxh@gmail.com',
-        rss: '/rss.xml',
-        // customized sns
-        bilibili: {
-          icon: 'ri-bilibili-line',
-          link: 'https://www.bilibili.com/',
-        },
+        github: 'shaul-xu',
+        email: 'xuyixiang18@gmail.com',
       },
     },
 
@@ -163,86 +154,30 @@ export default defineUserConfig<GungnirThemeOptions>({
       mermaid: true,
       chartjs: true,
       giscus: {
-        repo: 'This-is-an-Apple/gitalk-comments',
-        repoId: 'MDEwOlJlcG9zaXRvcnkyMTA1MjQyMTQ=',
-        category: 'Announcements',
-        categoryId: 'DIC_kwDODIxYNs4CAO1u',
+        repo: 'shaul-xu/blog',
+        repoId: 'MDEwOlJlcG9zaXRvcnkzOTc0ODYzMzk=',
+        category: 'General',
+        categoryId: 'DIC_kwDOF7EpA84COi_d',
         lazyLoad: true,
       },
       mdPlus: {
         all: true,
       },
-      ga: 'G-EE8M2S3MPB',
-      ba: '10b7bc420625758a319d6b23aed4700f',
-      rss: {
-        siteURL: 'https://v2.vuepress-theme-gungnir.vercel.app',
-        copyright: 'Renovamen 2018-2022',
-      },
+      ga: 'G-ZLWJGDQC5S',
+      ba: 'eeefe762518116b8686f71979df2f0b4',
       pwa: true,
-      search: false, // use @vuepress/plugin-docsearch instead
+      // search: false, // use @vuepress/plugin-docsearch instead
     },
 
     footer: `
-      &copy; <a href="https://github.com/Renovamen" target="_blank">Renovamen</a> 2018-2022
+      &copy; <a href="https://github.com/shaul-xu" target="_blank">Shaul</a> 2019-2022
       <br>
       Powered by <a href="https://v2.vuepress.vuejs.org" target="_blank">VuePress</a> &
       <a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
     `,
   },
 
-  plugins: [
-    // [
-    //   "@vuepress/plugin-docsearch",
-    //   {
-    //     appId: "3DDLGP0IG4",
-    //     apiKey: "6556adaa82b31485309b440a525f264a",
-    //     indexName: "v2-vuepress-theme-gungnir",
-    //     locales: {
-    //       "/zh/": {
-    //         placeholder: "搜索文档",
-    //         translations: {
-    //           button: {
-    //             buttonText: "搜索文档",
-    //             buttonAriaLabel: "搜索文档"
-    //           },
-    //           modal: {
-    //             searchBox: {
-    //               resetButtonTitle: "清除查询条件",
-    //               resetButtonAriaLabel: "清除查询条件",
-    //               cancelButtonText: "取消",
-    //               cancelButtonAriaLabel: "取消"
-    //             },
-    //             startScreen: {
-    //               recentSearchesTitle: "搜索历史",
-    //               noRecentSearchesText: "没有搜索历史",
-    //               saveRecentSearchButtonTitle: "保存至搜索历史",
-    //               removeRecentSearchButtonTitle: "从搜索历史中移除",
-    //               favoriteSearchesTitle: "收藏",
-    //               removeFavoriteSearchButtonTitle: "从收藏中移除"
-    //             },
-    //             errorScreen: {
-    //               titleText: "无法获取结果",
-    //               helpText: "你可能需要检查你的网络连接"
-    //             },
-    //             footer: {
-    //               selectText: "选择",
-    //               navigateText: "切换",
-    //               closeText: "关闭",
-    //               searchByText: "搜索提供者"
-    //             },
-    //             noResultsScreen: {
-    //               noResultsText: "无法找到相关结果",
-    //               suggestedQueryText: "你可以尝试查询",
-    //               openIssueText: "你认为该查询应该有结果？",
-    //               openIssueLinkText: "点击反馈"
-    //             }
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // ]
-  ],
+  plugins: [],
 
   markdown: {
     extractHeaders: {
---
+++ docs/posts/如何使 a == 1 && a == 2 && a == 3 为 true.md
@@ -0,0 +1,93 @@
+---
+layout: Post
+title: 如何使 a == 1 && a == 2 && a == 3 为 true
+# subtitle: A Test Post with a Header Image
+author: Shaul
+date: 2022-04-07
+useHeaderImage: true
+headerImage: /img/in-post/2022-04-07/cover.jpeg
+headerMask: rgba(40, 57, 101, .4)
+tags:
+  - JavaScript
+---
+
+<!-- more -->
+
+## 问题描述
+
+> 在 JavaScript 中，如何使 `(a == 1 && a == 2 && a == 3)` 返回 `true` ？
+
+## 解决方案
+
+### 方案一：使用 getter 存储器
+
+```js
+var n = 1
+
+Object.defineProperty(globalThis, 'a', {
+  get() {
+    return n++
+  },
+})
+
+a == 1 && a == 2 && a == 3 // true
+a === 1 && a === 2 && a === 3 // true
+```
+
+::: info
+
+使用 getter 存储器的方式，也就是以全局变量 `n` 存储一个值，每次调用的时候都使 `n = n + 1`。
+
+:::
+
+### 方案二：重写对象的 `valueOf()` 或 `toString()` 方法
+
+```js
+const a = {
+  value: 1,
+  valueOf() {
+    return this.value++
+  },
+}
+
+a == 1 && a == 2 && a == 3 // true
+```
+
+```js
+const a = {
+  value: 1,
+  toString() {
+    return a.value++
+  },
+}
+
+a == 1 && a == 2 && a == 3 // true
+```
+
+::: info <b> 这里的 <code>valueOf()/toString()</code> 为什么会被调用 ？</b>
+
+原因参考如下：
+
+1. 如果一个是 `null`，一个是 `undefined`，则它们相等。
+2. 如果一个是数字，一个是字符串，先将字符串转换成数字，然后使用转换后的值进行比较。
+3. 如果其中的一个值为 `true`，则转换成 1 再进行比较；如果其中一个值为 `false`，这转换成 `0` 再进行比较。
+4. 如果一个值是对象，另一个值是数字或者字符串，则将对象转换成原始值再进行比较。转换成字符串时，会先调用 `toString()`，如果没有`toString()` 方法或者返回的不是一个原始值，则再调用 `valueOf()`，如果还是不存在或者返回不是原始值，则会抛出一个类型错误的异常。返回的原始值会被转换成字符串；如果转换成数字时，也是类似的，不过是会先调用 `valueOf()`，再调用 `toString()`，返回的原始值会被转换成数字。
+5. 其他不同类型之间的比较均不相等。
+
+所以在这里使用 `a` 与这些字符进行比较时会被转换成数字，此时会默认调用字符串的 `valueOf()/toString()` 方法，我们将这个方法进行重写，用于拦截处理 `a` 的值。
+:::
+
+### 方案三：Proxy
+
+```js
+const a = new Proxy(
+  { i: 0 },
+  {
+    get(target, name) {
+      return name === Symbol.toPrimitive ? () => ++target.i : target[name]
+    },
+  }
+)
+
+a == 1 && a == 2 && a == 3 // true
+```
